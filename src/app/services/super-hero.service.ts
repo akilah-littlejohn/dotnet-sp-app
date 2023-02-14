69604c9d5ca48276f9ac200a4265bd172c7613ca
Akilah Littlejohn <akilahlittlejohn@gmail.com>
2023-02-14 10:42:21 -0600

    Create a SuperHeroe interface to define the structure of superhero objects and a method that pushes object into an array

---
+++ src/app/services/super-hero.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { SuperHeroe } from '../models/super-hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SuperHeroService {
+  private heroes: SuperHeroe[];
+
+  constructor() {
+    this.heroes = [];
+  }
+  getSuperHeroes() {
+    // tutorial created a class I decided to use an interface instead
+    const hero: SuperHeroe = {
+      id: 1,
+      name: 'Spider Man',
+      firstName: 'Peter',
+      lastName: 'Parker',
+      place: 'New York'
+
+    }
+
+    this.heroes.push(hero)
+    return this.heroes
+  }
+
+
+}
